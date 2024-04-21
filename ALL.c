8e0327bcc42597fc520b522034df026f7f89a1e5
cam <ayresc17@holyportcollege.org.uk>
2024-04-21 23:24:22 +0100

    Wrote a function to initialise the linked list.

--- ALL.c
+++ ALL.c
@@ -8,14 +8,37 @@
 
 
 typedef struct nodeStruct {
-    int data;
+    char data;
     struct nodeStruct *next;
     struct nodeStruct *prev;
 } node_t;
 
 
+node_t *llInit(char info);
+void llAppend(char info, node_t ll);
+
 
 int main(int argc, char *argv[])
 {
+    node_t *ll;
+
+    ll = llInit('c');
+    free(ll);
+    
     return(0);
+}
+
+
+node_t *llInit(char info)
+{
+    node_t *ll = malloc(sizeof(node_t));
+    ll->data = info;
+    ll->next = ll;
+    ll->prev = ll;
+    
+    return(ll);
+}
+
+void llAppend(char info, node_t ll)
+{
 }
