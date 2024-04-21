fb3490250fa6eef26fcd008ab424b2659e77b09f
cam <ayresc17@holyportcollege.org.uk>
2024-04-21 23:12:05 +0100

    Start on the main source file.

---
+++ ALL.c
@@ -0,0 +1,21 @@
+/* Advanced Linked List.
+
+   The general idea for this was described in the README.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct nodeStruct {
+    int data;
+    struct nodeStruct *next;
+    struct nodeStruct *prev;
+} node_t;
+
+
+
+int main(int argc, char *argv[])
+{
+    return(0);
+}
