f2b3189cf5547e51c59f6443948a8d6375d0fa0a
cam <ayresc17@holyportcollege.org.uk>
2024-04-21 23:37:00 +0100

    Started work on a function to get the final index, and I think that it works...

--- ALL.c
+++ ALL.c
@@ -17,20 +17,25 @@ typedef struct nodeStruct {
 
 
 node_t *llInit(char info);
-void llAppend(char info, node_t ll);
+void llAppend(char info, node_t *ll);
+node_t *llFinal(node_t *ll);
 
 
 int main(int argc, char *argv[])
 {
-    node_t *ll;
+    node_t *ll = llInit('c');
 
-    ll = llInit('c');
-    free(ll);
+    printf("%c\n", llFinal(ll)->data);
+    llAppend('a', ll);
+    llAppend('m', ll);
+    printf("%c\n", llFinal(ll)->data);
     
+    free(ll);
     return(0);
 }
 
 
+/* Create a new linked list. */
 node_t *llInit(char info)
 {
     node_t *ll = malloc(NODESIZE);
@@ -41,14 +46,28 @@ node_t *llInit(char info)
     return(ll);
 }
 
-void llAppend(char info, node_t ll)
+/* Append to an existsing linked list. */
+void llAppend(char info, node_t *ll)
 {
     node_t *node = malloc(NODESIZE);
-    note_t *llEnd = llFinal(ll);
+    node_t *llEnd = llFinal(ll);
 
     node->data = info;
     node->next = ll;
 
     node->prev = llEnd;
     llEnd->next = node;
+}
+
+/* So the idea for this guy is to store the address
+   of the first index of the ll, and then assume whatever
+   node has a pointer to that address is the final node.
+
+   Then, we return a pointer to that final node.
+*/
+node_t *llFinal(node_t *ll)
+{
+    node_t *nextNode = ll->next;
+    while(nextNode != ll) nextNode = nextNode->next;
+    return(nextNode);
 }
