e52b8b771a0d46457b26db96ab1aa2a64c2e6af6
cam <ayresc17@holyportcollege.org.uk>
2024-04-21 23:28:55 +0100

    Wrote an appender function, but now need an iterator function.

--- ALL.c
+++ ALL.c
@@ -6,6 +6,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NODESIZE sizeof(node_t)
+
 
 typedef struct nodeStruct {
     char data;
@@ -31,7 +33,7 @@ int main(int argc, char *argv[])
 
 node_t *llInit(char info)
 {
-    node_t *ll = malloc(sizeof(node_t));
+    node_t *ll = malloc(NODESIZE);
     ll->data = info;
     ll->next = ll;
     ll->prev = ll;
@@ -41,4 +43,12 @@ node_t *llInit(char info)
 
 void llAppend(char info, node_t ll)
 {
+    node_t *node = malloc(NODESIZE);
+    note_t *llEnd = llFinal(ll);
+
+    node->data = info;
+    node->next = ll;
+
+    node->prev = llEnd;
+    llEnd->next = node;
 }
